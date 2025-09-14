4e2da33fecdaeb035d78233038f3203d4851067b
Avinash <avimalhotra505@gmail.com>
2025-09-14 18:32:53 +0530

    two way data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Course } from './course/course';
+// import { Course } from './course/course';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Course, CommonModule],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   preserveWhitespaces:true
@@ -14,21 +15,9 @@ import { CommonModule } from '@angular/common';
 export class App {
   title="Angular";
   version=20;
-  alt="star";
-  src="favicon.ico";
-  dis=false;
-  p="lead";
-  count=0;
 
-  data=["swift","polo","audi","bmw"];
-  swift={name:"swift",power:82};
+  name="";
+  num=0;
 
-  // sayHi(){ return "Hello Angular" };
-  // sayHi(){console.log("Hello Angular") };
-
-  counter(){this.count=this.count+1}
-
- 
-  reverseString=(x:string)=> x.split("").reverse().join("");
-
+  
 }
