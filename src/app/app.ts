8c160a8b10b7c7f2ddbc8f9ebed5a48283a0323a
Avinash Malhotra <avimalhotra505@gmail.com>
2025-09-29 21:59:44 +0530

    reactive forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Course } from './course/course';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Login } from '../Login';
+import { FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormArray, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Course, CommonModule, FormsModule],
+  imports: [Course, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   preserveWhitespaces: true,
@@ -14,9 +16,26 @@ import { Login } from '../Login';
 export class App {
   title = 'Angular';
   version = 20;
-  model=new Login("",0,"","",false);
 
-  onSubmit(data:any){ console.log(data.value) }
+  // email=new FormControl('');
+  // password=new FormControl('');
+
+    loginform=new FormGroup({
+      email:new FormControl(null,[Validators.required, Validators.email]),
+      password:new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z0-9]{8,12}')]),
+    });
+
+
+    submitForm(){
+        // console.log( this.loginform.value) ;
+        console.log( this.loginform.value.email) ;
+        console.log( this.loginform.value.password) ;
+    }
+
+    constructor(){
+      console.log("constructor");
+    }
+
 
 
 }
